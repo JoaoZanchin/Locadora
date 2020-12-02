9203209f6b5fb2b5d26b8115e34f57439376e525
Jean <Jean@DESKTOP-KG9EIVA>
2020-12-02 15:24:57 -0300

    Aula do dia 02/10, Filme.

---
+++ src/view/JFListarFilmes.java
@@ -0,0 +1,103 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import model.bean.Filme;
+import model.dao.FilmeDAO;
+
+import javax.swing.JButton;
+
+public class JFListarFilmes extends JFrame {
+
+	private JPanel contentPane;
+	private JTable jtFilme;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFListarFilmes frame = new JFListarFilmes();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFListarFilmes() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 795, 540);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Listar Filmes");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(12, 13, 131, 29);
+		contentPane.add(lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(22, 55, 727, 344);
+		contentPane.add(scrollPane);
+		
+		jtFilme = new JTable();
+		jtFilme.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+				{null, null, null, null},
+				{null, null, null, null},
+			},
+			new String[] {
+				"idFilme", "T\u00EDtulo", "Categoria", "Tempo"
+			}
+		));
+		scrollPane.setViewportView(jtFilme);
+		
+		JButton btnCadastrar = new JButton("Cadastrar FIlme");
+		btnCadastrar.setBounds(12, 432, 131, 25);
+		contentPane.add(btnCadastrar);
+		
+		JButton btnAlterar = new JButton("Alterar Filme");
+		btnAlterar.setBounds(182, 432, 131, 25);
+		contentPane.add(btnAlterar);
+		
+		JButton btnExcluir = new JButton("Excluir Filme");
+		btnExcluir.setBounds(359, 432, 124, 25);
+		contentPane.add(btnExcluir);
+		
+		readJTable();
+	}
+	
+	public void readJTable() {
+		DefaultTableModel modelo = (DefaultTableModel) jtFilme.getModel();
+		modelo.setNumRows(0);
+		FilmeDAO fdao = new FilmeDAO();
+		for(Filme f : fdao.read()) {
+			modelo.addRow(new Object[] {
+					f.getIdFilme(),
+					f.getTitulo(),
+					f.getCategoria(),
+					f.getTempo()
+			});
+		}
+	}
+	
+}
